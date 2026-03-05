cbd6641d771843cd71a46fbfe94d3e6263055a94
Ishwar Vaidya <141947486+IshwarVaidya2531@users.noreply.github.com>
2026-03-05 13:07:13 +0530

    Time: 4 ms (42.06%), Space: 43.1 MB (86.02%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        char[] arr = s.toCharArray();
+        for(int i =0; i<arr.length;i++){
+            
+            if((arr[i] =='(') || (arr[i] == '{') || (arr[i]=='[')){
+                stack.push(arr[i]);
+            }else{
+                if(stack.isEmpty()) return false;
+                char poped = stack.pop();
+                if((arr[i] == ')' && poped != '(') ||
+                (arr[i]=='}' && poped != '{') || 
+                (arr[i]==']' && poped !='[')) return false;
+            }
+        }
+      return stack.isEmpty();
+        
+    }
+}
