2f91b914ed2617073b5be5acded288cea46326bc
Ishwar Vaidya <141947486+IshwarVaidya2531@users.noreply.github.com>
2026-03-30 14:54:30 +0530

    Time: 41 ms (93.53%), Space: 106.4 MB (66.17%) - LeetHub

--- 2260-minimum-consecutive-cards-to-pick-up/2260-minimum-consecutive-cards-to-pick-up.java
+++ 2260-minimum-consecutive-cards-to-pick-up/2260-minimum-consecutive-cards-to-pick-up.java
@@ -3,9 +3,9 @@ class Solution {
         Map<Integer, Integer> map = new HashMap<>();
         int res = Integer.MAX_VALUE;
         for(int i =0; i < cards.length;i++){
-            if(map.containsKey(cards[i]))
+            if(map.containsKey(cards[i])){
             res = Math.min(res, i - map.get(cards[i]) + 1);
-            
+            }            
             map.put(cards[i], i);
         }
         return res == Integer.MAX_VALUE ? -1 : res;
