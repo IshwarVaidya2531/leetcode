11786c21d47fa3e4a80ca1c8ee86f635dbc40604
Ishwar Vaidya <141947486+IshwarVaidya2531@users.noreply.github.com>
2026-04-21 11:23:25 +0530

    Time: 51 ms (72.54%), Space: 93.3 MB (35.53%) - LeetHub

--- 1695-maximum-erasure-value/1695-maximum-erasure-value.java
+++ 1695-maximum-erasure-value/1695-maximum-erasure-value.java
@@ -4,8 +4,8 @@ class Solution {
         Set<Integer> set = new HashSet<>();
         for(int right=0; right<nums.length;right++){
             while(set.contains(nums[right])){
-                set.remove(nums[left]);
                 sum -= nums[left];
+                set.remove(nums[left]);
                 left++;
             }
             set.add(nums[right]);
