668594d31b36f0830c858227ef66961757393038
Ishwar Vaidya <141947486+IshwarVaidya2531@users.noreply.github.com>
2026-05-08 19:27:00 +0530

    Time: 1 ms (91.86%), Space: 43.1 MB (37.13%) - LeetHub

--- 0791-custom-sort-string/0791-custom-sort-string.java
+++ 0791-custom-sort-string/0791-custom-sort-string.java
@@ -1,8 +1,9 @@
 class Solution {
     public String customSortString(String order, String s) {
         StringBuilder sb = new StringBuilder();
-        Set<Character> set = new HashSet<>();
+        HashSet set = new HashSet();
         int[] count = new int[26];
+
         for(char ch : order.toCharArray()){
             set.add(ch);
         }
@@ -10,15 +11,16 @@ class Solution {
             if(!set.contains(ch)){
                 sb.append(ch);
             }else{
-                count[ch - 'a']++;
+                count[ch -'a']++;
             }
         }
         for(char ch : order.toCharArray()){
-            int i = count[ch -'a'];
-            while(i-->0){
+            int i = count[ch - 'a'];
+            while(i-- > 0){
                 sb.append(ch);
             }
         }
+
         return sb.toString();
     }
 }
