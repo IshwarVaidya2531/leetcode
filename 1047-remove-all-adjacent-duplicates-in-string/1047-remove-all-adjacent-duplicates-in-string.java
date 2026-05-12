e32ac8a647fbc9c82f946fcd62ba440c55691de6
Ishwar Vaidya <141947486+IshwarVaidya2531@users.noreply.github.com>
2026-05-12 19:06:42 +0530

    Time: 11 ms (87.94%), Space: 47.2 MB (52.23%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.java
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.java
@@ -2,33 +2,33 @@ class Solution {
     public String removeDuplicates(String s) {
 
         // Approach 1
-        // StringBuilder res = new StringBuilder();
-        // if(s.length() == 1) return s;
-        // for(char c : s.toCharArray()){
-        //     if(res.length() > 0 && (c == res.charAt(res.length()-1))){
-        //         res.deleteCharAt(res.length()-1);
-        //     }else{
-        //         res = res.append(c);
-        //     }
-        // }
-        // return res.toString();
+        StringBuilder res = new StringBuilder();
+        if(s.length() == 1) return s;
+        for(char c : s.toCharArray()){
+            if(res.length() > 0 && (c == res.charAt(res.length()-1))){
+                res.deleteCharAt(res.length()-1);
+            }else{
+                res = res.append(c);
+            }
+        }
+        return res.toString();
 
         // Approach 2
 
-        Stack<Character> stack = new Stack<>();
-        StringBuilder sb = new StringBuilder();
-        for(char c : s.toCharArray()){
-            if(stack.isEmpty()){
-                stack.push(c);
-            }else if(c == stack.peek()){
-                 stack.pop();  
-            }else{   
-             stack.push(c); 
-            }
-        }
-        while(!stack.isEmpty()){
-            sb.append(stack.pop());
-        }
-        return sb.reverse().toString();
+        // Stack<Character> stack = new Stack<>();
+        // StringBuilder sb = new StringBuilder();
+        // for(char c : s.toCharArray()){
+        //     if(stack.isEmpty()){
+        //         stack.push(c);
+        //     }else if(c == stack.peek()){
+        //          stack.pop();  
+        //     }else{   
+        //      stack.push(c); 
+        //     }
+        // }
+        // while(!stack.isEmpty()){
+        //     sb.append(stack.pop());
+        // }
+        // return sb.reverse().toString();
     }
 }
