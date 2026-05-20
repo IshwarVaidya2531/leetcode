aa05468abd36975f3fd99a9c6bbcf291d3d22027
Ishwar Vaidya <141947486+IshwarVaidya2531@users.noreply.github.com>
2026-05-20 11:28:48 +0530

    Time: 21 ms (81.81%), Space: 59.4 MB (67.87%) - LeetHub

--- 0933-number-of-recent-calls/0933-number-of-recent-calls.java
+++ 0933-number-of-recent-calls/0933-number-of-recent-calls.java
@@ -2,22 +2,37 @@ class RecentCounter {
     
     // Approach 1
 
-            List<Integer> list;
-           int i;
-            int j;
-            public RecentCounter() {
-                 list = new ArrayList();
-                 i =0;
-                 j =-1;
+        //     List<Integer> list;
+        //    int i;
+        //     int j;
+        //     public RecentCounter() {
+        //          list = new ArrayList();
+        //          i =0;
+        //          j =-1;
+        //     }
+        //     public int ping(int t) {
+        //       j++;
+        //       list.add(t);
+        //       while(i<j && (list.get(j) - 3000 > list.get(i))){
+        //          i++;
+        //      }
+        //      return j - i +1;
+        //      }
+    
+    // Approach 2 
+
+     Queue<Integer> queue;
+       public RecentCounter() {
+            queue = new LinkedList();
+        }
+
+        public int ping(int t) {
+            queue.add(t);
+            while(!queue.isEmpty() && (t - 3000 > queue.peek())){
+                queue.poll();
             }
-            public int ping(int t) {
-              j++;
-              list.add(t);
-              while(i<j && (list.get(j) - 3000 > list.get(i))){
-                 i++;
-             }
-             return j - i +1;
-             }
+            return queue.size();
+        }
         
 
 }
